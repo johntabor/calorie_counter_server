f0123d3deb5d85853f073d72fb1e53f1b7743d9b
john <johntabor@outlook.com>
2020-05-22 09:19:36 -0400

    fixed config stuff for local branch

--- app.js
+++ app.js
@@ -3,18 +3,10 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const pgp = require('pg-promise')();
 const bcrypt = require('bcryptjs');
-//const cn = require('./config')
+const cn = require('./config')
 const SALT_ROUNDS = 10
 
 /* postgres config */
-
-const cn = {
-    connectionString: process.env.DATABASE_URL
-    /* port: 5432,
-    database: 'test',
-    user: 'john',
-    password: 'Football55', */
-}; 
 const db = pgp(cn)
 
 /* express config */
